a5754d14bc3724e3b87d5c3e57150e74e733ec63
pipegomez2009@hotmail.com <vale_tina304@hotmail.com>
2016-10-23 17:34:51 -0500

    Se agrega proyecto de android estudio

---
+++ Vozarron/app/src/main/java/com/uniquindio/proyecto/android/electiva/vozarron/PrincipalActivity.java
@@ -0,0 +1,13 @@
+package com.uniquindio.proyecto.android.electiva.vozarron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pricipanl);
+    }
+}
