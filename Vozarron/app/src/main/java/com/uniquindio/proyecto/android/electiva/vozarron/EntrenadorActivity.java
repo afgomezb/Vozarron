a5666d12d162a25d6c94857072b5c3d94db46128
pipegomez2009@hotmail.com <vale_tina304@hotmail.com>
2016-10-24 02:18:49 -0500

    Se gregaron componentes a las vistas

--- Vozarron/app/src/main/java/com/uniquindio/proyecto/android/electiva/vozarron/EntrenadorActivity.java
+++ Vozarron/app/src/main/java/com/uniquindio/proyecto/android/electiva/vozarron/EntrenadorActivity.java
@@ -1,7 +1,9 @@
 package com.uniquindio.proyecto.android.electiva.vozarron;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EntrenadorActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class EntrenadorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrenador);
+    }
+
+    /**
+     * Metodo encargado de pasar a la ventana participantes
+     *
+     * @param view
+     */
+    public void pasarAParticipantes(View view) {
+        Intent intent = new Intent(this, ParticipantesActivity.class);
+        startActivity(intent);
     }
 }
